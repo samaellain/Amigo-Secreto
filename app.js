0fd9f4671e46a019f92dd4b8e3b3099d71a4b2cd
Samael <dark.samael@gmail.com>
2025-08-25 21:39:49 -0300

    Implementação da lógica segundo os requisitos. Primeiro teste

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    nome = document.getElementById("amigo").value;
+    if (nome !== "") {
+        amigos.push(nome);
+        document.getElementById("amigo").value = "";
+        document.getElementById("listaAmigos").innerHTML =  "";
+        for (let i = 0; i < amigos.length; i++) {
+            document.getElementById("listaAmigos").innerHTML += `<li>${amigos[i]}</li>`;
+        }
+    } else {
+        alert("Por favor, insira um nome válido.");
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Adicione pelo menos dois amigos para sortear.");
+        return;
+    }
+    let Sorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[Sorteado];
+    document.getElementById("resultado").innerHTML = `O amigo sorteado foi: <strong>${amigoSorteado}</strong>`;
+}
